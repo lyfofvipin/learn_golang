7edc25e694694e5ffcae87d6611e95ba59a2dae4
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2023-04-30 16:55:11 +0530

    Adding changes for Day04

---
+++ Day04/multi_line_comment.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt")
+
+func main(){
+
+	/*
+		this is a multiline test comment
+		line 2
+		line 3
+	*/
+
+	fmt.Println("Vipin Kumar")
+}
---
+++ Day04/single_line_comments.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt")
+
+func main(){
+
+	// This is a comment
+	fmt.Println("Vipin")
+	// This is also an comment
+	//  This os also an comment
+	fmt.Println("Kumar")
+}
